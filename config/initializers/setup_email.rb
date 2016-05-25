92e4149e666c106218acabf54637d15019363c48
palong88 <patrick@plongmail.info>
2016-05-25 09:53:49 +0100

    Send Grid Configured: Renames set_upgmail to setup_email

---
+++ config/initializers/setup_email.rb
@@ -0,0 +1,30 @@
+
+#Uses Gmail local and Sendgrid on production
+if Rails.env.development?
+	ActionMailer::Base.delivery_method = :smtp
+	ActionMailer::Base.smtp_settings = {
+	  address:              'smtp.gmail.com',
+	  port:                 587,
+	  domain:               'ancient-bastion-57550.herokuapp.com',
+	  user_name:            'patrick@olearylong.com',
+	  password:             'pa_thax_54T',
+	  authentication:       'plain',
+	  enable_starttls_auto: true  }
+
+elseif Rails.env.production?
+
+  ActionMailer::Base.delivery_method = :smtp
+	ActionMailer::Base.smtp_settings = {
+	  address:              'smtp.sendgrid.net',
+	  port:                 587,
+	  domain:               'heroku.com',
+	  user_name:            ENV["SENDGRID_USERNAME"],
+	  password:             ENV["SENDGRID_PASSWORD"],
+	  authentication:       'plain',
+	  enable_starttls_auto: true  }
+end
+
+
+
+
+
--- config/initializers/setup_gmail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'ancient-bastion-57550.herokuapp.com',
-  user_name:            'patrick@olearylong.com',
-  password:             'pa_thax_54T',
-  authentication:       'plain',
-  enable_starttls_auto: true  }
