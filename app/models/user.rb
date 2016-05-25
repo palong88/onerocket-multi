e8d1b8bccaba62c2eb2623f3f4c87e4e5c9d6ddf
palong88 <patrick@plongmail.info>
2016-05-25 09:22:54 +0100

    Error for log in. Account Model Added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+   validate :email_is_unique, on: :create
+   after_create :create_account
+
+
+
+   private
+
+   #Email should be unique in the account model
+   def email_is_unique
+   	#Do Not validate email if errors are already in devise or present.
+   	return false unless self.errors[:email].empty?
+
+   	unless Account.find_by_email(email).nil?
+   		errors.add(:email, "Email is already in use")
+   	end
+   end
+   #Creates accounta after new user account
+   #Saves users email to account model
+   def create_account
+      account = Account.new(:email => email)
+      account.save!
+   end
+
+
+
 end
