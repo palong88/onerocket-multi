24ad1e8db542dafd5d0e5611c7305fbf3e00e895
palong88 <patrick@plongmail.info>
2016-05-27 19:29:52 +0100

    Changed www to ap. added main domain of app.onerocket.io

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < Devise::SessionsController
   #Overriding Devise's session controller to check for subdomain 
   #Only allow login if subdomain is present and is not www
   def new 
-    if request.subdomain.blank? || request.subdomain == "www"
+    if request.subdomain.blank? || request.subdomain == "app"
         flash[:notice] = "Access Restricted"
         redirect_to :root
     else
