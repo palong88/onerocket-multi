9d0d2f38883a369ba064842ead4b62d6f1fee979
Peter Marcano <pbmarcano91@gmail.com>
2016-06-21 12:18:04 -0400

    new tattle tale feature rats out users that have tasks that are 3 days overdue to their admins!

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -6,4 +6,11 @@ class NotificationMailer < ApplicationMailer
     @url  = 'https://' + @user.subdomain + ".onerocket.io/eadmin_tasks"
     mail(to: @user.email, subject: 'You have one or more overdue tasks!')
   end
+
+  def tattle_tale(user, employee)
+    @user = user
+    @employee = employee
+    @url  = 'https://' + @user.subdomain + ".onerocket.io/users"
+    mail(to: user.email, subject: @employee.name + " is overdue on one or more tasks")
+  end
 end
