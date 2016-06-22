f85ffd6d6cf25cffe6cabfb6d38de991fc44f8f5
palong88 <patrick@plongmail.info>
2016-06-22 12:51:08 +0200

    Add/Cancel and Update are working with single bug.
    #Bug: Card has to be entered twice before a token is created.
    
    Not sure how this is happening as I removed the JS that creates the
    token from each from and the asset pipeline.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,6 +1,6 @@
 class SubscriptionsController < ApplicationController
 
-    # skip_before_filter :verify_authenticity_token
+    skip_before_filter :verify_authenticity_token
     before_action :authenticate_user!
     before_action :authorize_user
 
@@ -38,10 +38,11 @@ class SubscriptionsController < ApplicationController
         customer.source = token
         customer.save
 
-        redirect_to "/subscriptions", :notice => "Card updated successfully"
+        redirect_to edit_user_registration_path, :notice => "Card updated successfully"
 
-    rescue => e
-        redirect_to :action => "update_card", :flash => { :notice => e.message }
+        rescue => e
+            redirect_to :action => "update_card", :notice => "Card not updated, Please try again."
+
 
 
     end
@@ -56,7 +57,8 @@ class SubscriptionsController < ApplicationController
         stripe_plan_id  = current_account.stripe_plan_id
         current_plan = current_account.stripe_plan_id
         ap token
-        if customer_id.nil? || stripe_plan_id.nil?
+
+        if customer_id.nil?
             #New customer -> Create a customer
             #Create a Customer
             @customer = Stripe::Customer.create(
@@ -68,11 +70,11 @@ class SubscriptionsController < ApplicationController
             subscriptions    = @customer.subscriptions
             @subscribed_plan = subscriptions.data.find{|o| o.plan.id == plan}
 
-        else
-            #Customer exists
-            #Get customer from Stripe
-            @customer = Stripe::Customer.retrieve(customer_id)
-            @subscribed_plan = create_or_update_subscription(@customer, current_plan, plan)
+        # else
+        #     #Customer exists
+        #     #Get customer from Stripe
+        #     @customer = Stripe::Customer.retrieve(customer_id)
+        #     @subscribed_plan = create_or_update_subscription(@customer, current_plan, plan)
 
         end
 
@@ -82,13 +84,11 @@ class SubscriptionsController < ApplicationController
         active_until = Time.at(current_period_end).to_datetime
 
         save_account_details(current_account, plan, @customer.id, active_until)
-
-
-
-        redirect_to :root, :notice => "Successfully subscribed to a plan" #Plan-name
+        #redirects to Edit registration account for the user with notice.
+        redirect_to edit_user_registration_path, :notice => "Successfully added credit card" #Plan-name
 
      rescue => e
-        redirect_to :back, :flash => {:error => e.message }
+        redirect_to :back, :notice => "Card not added. Please try again."
 
     end
 
@@ -99,33 +99,19 @@ class SubscriptionsController < ApplicationController
         customer_id     = current_account.customer_id
         current_plan    = current_account.stripe_plan_id
 
-        if current_plan.blank?
-            raise "No plan found to unsubscribe/cancel"
-        end
-
         #Fetch customer from Stripe
         customer = Stripe::Customer.retrieve(customer_id)
-
-        #Get customer's subscriptions
-        subscriptions = customer.subscriptions
-
-        #Find the subscription that we want to cancel
-        current_subscribed_plan = subscriptions.data.find { |o| o.plan.id == current_plan }
-
-        if current_subscribed_plan.blank?
-            raise "Subscription not found!!"
-        end
-
         #Delete it
-        current_subscribed_plan.delete
-
+        customer.delete
         #Update account model
-        save_account_details(current_account, "", customer_id, Time.at(0).to_datetime)
-
-        @message = "Subscription cancelled successfully"
+        save_account_details(current_account, nil, nil, Time.at(0).to_datetime)
+        ap "saved details"
+        ap current_account
+        @message = "Card Removed successfully"
+        redirect_to edit_user_registration_path, :notice => "Card removed successfully"
 
     rescue => e
-        redirect_to "/subscriptions", :flash => {:error => e.message}
+        redirect_to edit_user_registration_path, :flash => {:error => e.message}
     end
 
     def save_account_details(account, plan, customer_id, active_until)
@@ -136,30 +122,30 @@ class SubscriptionsController < ApplicationController
         account.save!
     end
 
-    #Doc
-    def create_or_update_subscription(customer, current_plan, new_plan)
-        subscriptions = customer.subscriptions
-
-        #Get current subscriptions
-        current_subscription = subscriptions.data.find{ |o| o.plan.id == current_plan }
-
-        if current_subscription.blank?
-            #No current subscription
-            #Maybe the customer unsubscribed previously or the card was declined
-            #So, create a new subscription to existing customer
-            subscription = customer.subscriptions.create({:plan => new_plan })
-        else
-            #Existing subscription found
-            #Must be an upgrade or a downgrade
-            #So update eistig subscription with new plan
-            current_subscription.plan = new_plan
-            subscription = current_subscription.save
-        end
-
-        return subscription
-
-
-    end
+    # #Doc
+    # def create_or_update_subscription(customer, current_plan, new_plan)
+    #     subscriptions = customer.subscriptions
+    #
+    #     #Get current subscriptions
+    #     current_subscription = subscriptions.data.find{ |o| o.plan.id == current_plan }
+    #
+    #     if current_subscription.blank?
+    #         #No current subscription
+    #         #Maybe the customer unsubscribed previously or the card was declined
+    #         #So, create a new subscription to existing customer
+    #         subscription = customer.subscriptions.create({:plan => new_plan })
+    #     else
+    #         #Existing subscription found
+    #         #Must be an upgrade or a downgrade
+    #         #So update eistig subscription with new plan
+    #         current_subscription.plan = new_plan
+    #         subscription = current_subscription.save
+    #     end
+    #
+    #     return subscription
+    #
+    #
+    # end
 
 
     #Authorize user or raise exception
