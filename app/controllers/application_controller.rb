6e9acd8bad2b01019e600bdca6e6d880d9ce6715
Peter Marcano <pbmarcano91@gmail.com>
2016-06-22 22:23:10 -0400

    fixed the form and the horrible indentation on some ruby files.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,43 +6,43 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :set_mailer_host
 
-    rescue_from CanCan::AccessDenied do |exception|
+  rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
 
 
 
-    protect_from_forgery with: :exception
-    before_filter :configure_permitted_parameters, if: :devise_controller?
-    protected
+  protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  protected
 
-      def authenticate_inviter!
-        unless current_user.has_role? :admin
-            redirect_to root_url, :alert => "You are not authorized to access this page."
-            return
-        end
-        super
-      end
-
-
-        def set_mailer_host
-          ActionMailer::Base.default_url_options[:host] = request.host_with_port
-              $url_host = request.host_with_port
-
-          end
-
-        def configure_permitted_parameters
-            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :start_date, :subdomain) }
-            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :start_date, :subdomain) }
-            devise_parameter_sanitizer.for(:new) { |u| u.permit(:name, :email, :password, :current_password, :start_date, :subdomain) }
-            devise_parameter_sanitizer.for(:invite)  << :subdomain << :start_date << :name
-        end
+  def authenticate_inviter!
+    unless current_user.has_role? :admin
+      redirect_to root_url, :alert => "You are not authorized to access this page."
+      return
+    end
+    super
+  end
 
 
-        def current_user_now
-          @current_user_now ||= session[:current_user_id] && User.find_by_id(session[:current_user_id]) # Use find_by_id to get nil instead of an error if user doesn't exist
-        end
-        helper_method :current_user_now #make this method available in views
+  def set_mailer_host
+    ActionMailer::Base.default_url_options[:host] = request.host_with_port
+    $url_host = request.host_with_port
+
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :start_date, :subdomain) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :start_date, :subdomain) }
+    devise_parameter_sanitizer.for(:new) { |u| u.permit(:name, :email, :password, :current_password, :start_date, :subdomain) }
+    devise_parameter_sanitizer.for(:invite)  << :subdomain << :start_date << :name << :user_info
+  end
+
+
+  def current_user_now
+    @current_user_now ||= session[:current_user_id] && User.find_by_id(session[:current_user_id]) # Use find_by_id to get nil instead of an error if user doesn't exist
+  end
+  helper_method :current_user_now #make this method available in views
 
 
 
