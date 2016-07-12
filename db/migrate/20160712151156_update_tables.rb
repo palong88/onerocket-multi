d7e5e6161d0d15c2fdd1af6f20ca19700918794e
palong88 <patrick@plongmail.info>
2016-07-12 16:39:43 +0100

    Updated Migration File

--- db/migrate/20160712151156_update_tables.rb
+++ db/migrate/20160712151156_update_tables.rb
@@ -20,7 +20,22 @@ class UpdateTables < ActiveRecord::Migration
   end
 
   def down
-    remove_column :users, :status
+
+    team = Team.find_by(name: "All")
+    team.destroy
+
+    category  = Category.where( :name => "Paperwork", :team => "All")
+    category2 = Category.where( :name => "Equipment & Tools", :team => "All")
+    category3 = Category.where( :name => "Meet the Company", :team => "All")
+    category4 = Category.where( :name => "Get Going", :team => "All")
+
+    category.destroy_all
+    category2.destroy_all
+    category3.destroy_all
+    category4.destroy_all
+
+    AdminTask.update_all(:team => nil )
+
   end
 
 end
