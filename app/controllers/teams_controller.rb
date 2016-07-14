d6a85f6bda92ceef332f01f458f97440cbbb0638
palong88 <patrick@plongmail.info>
2016-07-14 10:27:47 +0100

    Create newTeams Patch
    Redirects to create a new category for the new team.
    
    And Humanised output on admin task page(Category titles and team names.
    )

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -29,7 +29,7 @@ class TeamsController < ApplicationController
 
     respond_to do |format|
       if @team.save
-        format.html { redirect_to @team, notice: 'Team was successfully created.' }
+        format.html { redirect_to new_category_path(:team => @team.name ), notice: 'Team was successfully created. Please create a Category' }
         format.json { render :show, status: :created, location: @team }
       else
         format.html { render :new }
