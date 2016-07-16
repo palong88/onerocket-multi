9d996d39b92a45d175f5d436156c8314b33e9fad
palong88 <patrick@plongmail.info>
2016-07-16 03:31:00 +0100

    Index updated and Icons added per type of doc.

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,9 +2,19 @@ class Document < ActiveRecord::Base
   belongs_to :team
 
   has_attached_file :file
-  validates_attachment :file, content_type: { content_type: "application/pdf" }
-  #do_not_validate_attachment_file_type :document
-  #validates_attachment :document, :presence => true, :content_type => { :content_type => "application/pdf" }
-  #validate :check_document_present
+  validates_attachment :file, content_type: { content_type: "application/pdf" },   :styles => {
+    :thumb => ["200x200>", :pdf],
+    :medium => ["500x500>", :png]}
+
+
+    FileIcon = {
+
+        :pdf => '/pdf.png',
+
+        :doc => '/word.png'
+
+        #... and other any file types, all like this
+
+        }
 
 end
